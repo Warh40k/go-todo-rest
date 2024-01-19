b24259f49ab6d248467add96148ded1cef19ab58
nikita <nicita.zinkevitch@yandex.ru>
2024-01-19 10:29:47 +0300

    Lesson 7 completed

---
+++ pkg/handler/auth.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"github.com/Warh40k/go-todo-rest"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.Authorization.CreateUser(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+}
+
+func (h *Handler) signIn(c *gin.Context) {
+	var input todo.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
+}
